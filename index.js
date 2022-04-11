58c84dc39aba2f761ee8a399202389ffa28d38fd
Jennie Mauch <jenniemauch@me.com>
2022-04-11 00:05:01 -0400

    name file moved

---
+++ index.js
@@ -0,0 +1,26 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+console.log(inquirer)
+/*inquirer
+    .prompt([
+
+    ])
+    .then(answers => {
+
+    });*/
+
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
+
+
+//inquirer.prompt(questions) -> promise
